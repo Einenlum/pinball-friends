d48b7e1d2b17982b5be09463cefee88de6318a5d
Einenlum <contact@einenlum.com>
2024-01-24 00:14:31 +0100

    Fix typo

--- tests/Unit/HumanizeValueTest.php
+++ tests/Unit/HumanizeValueTest.php
@@ -5,14 +5,14 @@ it('humanizes values', function(string $locale, int $value, string $expected) {
 })->with([
     ['fr-FR', 0, '0'],
     ['fr-FR', 100, '100'],
-    // This space is a non breakable spaces (nbsp)
+    // This space is a non breakable space (nbsp)
     ['fr-FR', 100_000, '100 000'],
     ['fr-FR', 1_000_000, '1,000 million'],
     ['fr-FR', 1_200_000, '1,200 million'],
     ['fr-FR', 1_228_123, '1,228 million'],
     ['fr-FR', 2_000_000, '2,000 millions'],
     ['fr-FR', 2_123_900, '2,124 millions'],
-    // This space is a non breakable spaces (nbsp)
+    // This space is a non breakable space (nbsp)
     ['fr-FR', 274_543_123_900, '274 543,124 millions'],
 
     ['en', 0, '0'],
