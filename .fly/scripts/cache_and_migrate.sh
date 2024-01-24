75258cdd2938d74c0dfb969ef913c68604cbd54d
Einenlum <contact@einenlum.com>
2024-01-24 22:20:54 +0100

    If demo, reset database and seed

--- .fly/scripts/cache_and_migrate.sh
+++ .fly/scripts/cache_and_migrate.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
 /usr/bin/php /var/www/html/artisan optimize:clear --no-ansi -q -n
-/usr/bin/php /var/www/html/artisan migrate -n --force
+
+if [ "$APP_ENV" == "demo" ]
+then
+    /usr/bin/php /var/www/html/artisan migrate:fresh -n --force
+    /usr/bin/php /var/www/html/artisan db:seed --class="Database\Seeders\DemoSeeder" -n --force
+else
+    /usr/bin/php /var/www/html/artisan migrate -n --force
+fi
